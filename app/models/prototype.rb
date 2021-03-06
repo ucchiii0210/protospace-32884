65f38cd52c6a9725c1a07a3d731f630a00fdb44b
ucchiii0210 <y09052842409@icloud.com>
2021-03-06 10:39:03 +0900

    first commit

---
+++ app/models/prototype.rb
@@ -0,0 +1,10 @@
+class Prototype < ApplicationRecord
+
+  has_one_attached :image
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+
+  validates :catch_copy , presence: true
+  validates :concept , presence: true
+  validates :image , presence: true
+end
