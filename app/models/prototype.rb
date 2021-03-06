6b66d3e332dbed2cf68663f7576ca2fdd7c1af5c
ucchiii0210 <y09052842409@icloud.com>
2021-03-06 12:28:49 +0900

    prototype　titleバリデーション

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,6 +4,7 @@ class Prototype < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
+  validates :title , presence: true
   validates :catch_copy , presence: true
   validates :concept , presence: true
   validates :image , presence: true
